4fd101d11e6671fdb720547cda60de971d5ad001
Shiranka Miskin <shiranka.miskin@gmail.com>
2016-10-10 21:13:54 -0400

    Added hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
+const webpack = require('webpack');
+
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index.tsx',
+  ],
   output: {
-    filename: "./dist/bundle.js",
+    filename: './dist/bundle.js',
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: 'source-map',
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 
   module: {
     loaders: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      { test: /\.tsx?$/, loader: 'ts-loader' }
     ],
 
     preLoaders: [
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" }
+      { test: /\.js$/, loader: 'source-map-loader' }
     ]
-  },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
   },
 };
