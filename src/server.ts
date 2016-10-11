eaa23e3022fdd8d978636c808443692577f7e116
Shiranka Miskin <shiranka.miskin@gmail.com>
2016-10-10 23:24:37 -0400

    Removed swap files and edited package.json script

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var socket = require('socket.io');
+var io = socket(http);
 
 app.get('/', function(req: any, res: any){
-  res.send('<h1>Bye world</h1>');
+  res.send('<h1>Hi world</h1>');
 });
 
 http.listen(3000, function(){
