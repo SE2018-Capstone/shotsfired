a1bfb27577784f955ccdcc4380be68faa1220d6c
Sam Maier <sam@sammaier.ca>
2016-10-13 15:20:15 -0400

    merged

--- src/server.ts
+++ src/server.ts
@@ -12,3 +12,25 @@ app.get('/', function (req: express.Request, res: express.Response) {
 server.listen(3000, function () {
   console.log('listening on *:3000');
 });
+/*
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req: any, res: any){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket: any){
+  console.log('a user connected!');
+
+  socket.on('disconnect', function(){
+    console.log('user disc');
+  });
+
+  socket.on('message chat', function(msg: string) {
+    console.log('message: ' + msg);
+    io.emit('update!', msg);
+    socket.broadcast.emit('update!!', 'dat boi sent a msg: ' + socket.io.engine.id);
+  });
+*/
