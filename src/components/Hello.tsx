4e12398baf964fbd366c4a4698e71e6e75caf332
Shiranka Miskin <shiranka.miskin@gmail.com>
2016-10-13 18:34:56 -0400

    Added working socket code

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
+import * as io from 'socket.io-client';
 
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
+  input: React.Component;
+  socket: any;
+
+  constructor(props) {
+    super(props);
+    var socket = io('localhost:4000');
+    socket.on('chat message', function(msg){
+      console.log(msg);
+    });
+    socket.on('connect', function(){});
+    socket.on('event', function(data){});
+    socket.on('disconnect', function(){});
+    this.socket = socket;
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.socket.emit('chat message', this.input.value);
+    this.input.value = '';
+  }
+
   render() {
     return <h1>
-      Bye from {this.props.compiler} and {this.props.framework}!
+      <div>
+        Hello from {this.props.compiler} and {this.props.framework}!
+      </div>
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <input ref={(i) => this.input = i}/>
+      </form>
     </h1>;
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
+
 import * as express from 'express';
 import * as path from 'path';
 import * as http from 'http';
+import * as socketio from 'socket.io';
 var app = express();
-var server = http.createServer(app);
+var server = http.Server(app);
+var io = socketio(server);
 
 app.use(express.static(path.resolve(__dirname, '../')));
 app.get('/', function (req: express.Request, res: express.Response) {
   res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
-server.listen(3000, function () {
-  console.log('listening on *:3000');
+io.on('connection', function(socket: any){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(4000, function () {
+  console.log('listening on *:4000');
 });
