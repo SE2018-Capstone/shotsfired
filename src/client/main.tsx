ab625610fc22f35b529142dc956dd3f99823c87a
Sam Maier <sam@sammaier.ca>
2016-10-15 01:21:37 -0400

    Basic socket connection

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
+import * as socketIo from 'socket.io-client';
 import { GameCanvas } from './game-canvas';
 import { Game } from '../core/game';
 
 export class Main extends React.Component<{}, {}> {
   game: Game;
+  socket: SocketIOClient.Socket;
   
   constructor() {
     super();
     this.game = new Game();  
+    this.socket = socketIo('localhost:3000');
+    this.socket.on('state update', function(update:any) {
+      console.log(update);
+    });
   }
   
   render() {
