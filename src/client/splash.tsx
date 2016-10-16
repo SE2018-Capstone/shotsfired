bec4a4e7f6646183ea96df164f26d302aff459d0
Dhruv Lal <dlal295@gmail.com>
2016-10-16 13:29:39 -0400

    created class for splash screen

---
+++ src/client/splash.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class Splash extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div id="splash-screen">SPLASH</div>
+        );
+    }
+}
