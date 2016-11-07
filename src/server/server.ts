ae7f897ea23831b056fb905c60337305f5afe984
Sam Maier <sam@sammaier.ca>
2016-11-06 20:35:51 -0500

    Attempting to fix deployment

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,8 @@ import * as path from 'path';
 import * as http from 'http';
 import * as SocketIO from 'socket.io';
 import { LobbyServer } from './lobby-server';
+import * as process from 'process';
+
 var app = express();
 var server = http.createServer(app);
 
@@ -11,7 +13,7 @@ app.get('/', function (req: express.Request, res: express.Response) {
   res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
-server.listen(3000, function () {
+server.listen(process.env.PORT, function () {
   console.log('listening on *:3000');
 });
 
