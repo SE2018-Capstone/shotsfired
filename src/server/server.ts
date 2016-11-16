fbc1dc2c14f828b9062c8b47e4db5cd18910c34c
Sam Maier <sam@sammaier.ca>
2016-11-16 16:57:20 -0500

    Pushable deployment (#21)
    * All changes needed for deployment that make sense to merge to master.
    
    * All changes needed for deployment that make sense to merge to master.
    
    * Making port work locally
    
    * Updating console log

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,8 @@ import * as path from 'path';
 import * as http from 'http';
 import * as SocketIO from 'socket.io';
 import { LobbyServer } from './lobby-server';
+import * as process from 'process';
+
 var app = express();
 var server = http.createServer(app);
 
@@ -11,8 +13,9 @@ app.get('/', function (req: express.Request, res: express.Response) {
   res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
-server.listen(3000, function () {
-  console.log('listening on *:3000');
+server.listen(process.env.PORT || 3000, function () {
+  console.log('Port given by process.env variable: ', process.env.PORT);
+  console.log('Listening on port ', process.env.PORT || 3000);
 });
 
 new LobbyServer(server);
