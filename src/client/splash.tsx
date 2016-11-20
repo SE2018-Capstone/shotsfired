2bc2058011856d072d29e356046aa3f2dc9e2b67
Dhruv Lal <dlal295@gmail.com>
2016-11-20 16:45:14 -0500

    updated main menu display

--- src/client/splash.tsx
+++ src/client/splash.tsx
@@ -5,40 +5,102 @@ interface SplashProps {
 }
 export class Splash extends React.Component<SplashProps, {}> {
   render() {
+    const textColour = '#FFFFFF';
+    const buttonColour = '#008CBA';
     return (
       <div
         id="splash-screen"
         style={{
+          fontSize: 20,
           textAlign: 'center',
-          color: '#FFFFFF',
+          color: textColour,
           backgroundColor: '#000000',
           width: 640,
-          height: 480
+          height: 480,
+          paddingTop: 20
         }}>
-        <h1> SHOTS FIRED! </h1>
-        <button
-          onClick={this.props.onQuickPlay}
-          style={{
-            backgroundColor: '#008CBA',
-            color: "#FFFFFF",
-            fontSize: 28,
-            width: 240,
-            height: 60
-          }}>
-          Quick Play
-        </button>
-        <br/><br/>
-        <button
-          onClick={this.props.onQuickPlay}
-          style={{
-            backgroundColor: '#008CBA',
-            color: "#FFFFFF",
-            fontSize: 28,
-            width: 240,
-            height: 60
-          }}>
-          Private Match
-        </button>
+        <div id="title-left" style={{float: 'left'}}>
+          <img src="../../res/shot.png" style={{width: '30%', height: '30%'}}/>
+        </div>
+        <div id="title-right" style={{float: 'right'}}>
+          <img src="../../res/shot.png" style={{width: '30%', height: '30%'}}/>
+        </div>
+        <div id="title-center" style={{fontSize: 40, fontWeight: 'bold', paddingBottom: 5}} >
+          SHOTS FIRED!
+        </div>
+        <hr style={{color: textColour}} />
+        
+        <div id="left" style={{width: '50%', float: 'left', paddingTop: 35}}>
+          <div id="mode-text" style={{paddingLeft: 20, paddingRight: 20}}>
+            Play against random people
+            <br/>
+            in this 2-4 person mode
+          </div>
+          <br/>
+          <img src="../../res/2-player-icon.png" style={{width: 75, height: 60}} />
+          <br/> TO <br/>
+          <img
+            src="../../res/2-player-icon.png"
+            style={{
+              width: 75,
+              height: 60,
+              paddingTop: 5,
+              marginRight: -11
+            }}
+          />
+          <img
+            src="../../res/2-player-icon.png"
+            style={{
+              width: 75,
+              height: 60,
+              paddingTop: 5
+            }}
+          />
+
+        </div>
+
+        <div id="right" style={{width: '50%', float: 'right', paddingTop: 35}}>
+          <div id="mode-text" style={{paddingLeft: 20, paddingRight: 20}}>
+            Play with friends by
+            <br/>
+            getting a shareable URL
+            <br/>
+            to join the same party
+          </div>
+          <br/>
+          <img src='../../res/2-player-icon.png' style={{width: 125, height: 100}} />
+          <h3 style={{marginTop: -10}} > WWW. </h3>
+        </div>
+        <br style={{clear: 'both'}} />
+        <div id="button-container" style={{textAlign: 'center', marginTop: 10}}>
+          <div id="left-button" style={{float: 'left', width: '50%'}}>
+            <button
+              onClick={this.props.onQuickPlay}
+              style={{
+                backgroundColor: buttonColour,
+                color: textColour,
+                fontSize: 28,
+                width: 240,
+                height: 60
+              }}>
+              Quick Play
+            </button>
+          </div>
+          <div id="right-button" style={{float: 'right', width: '50%'}}>
+            <button
+              onClick={this.props.onQuickPlay}
+              style={{
+                backgroundColor: buttonColour,
+                color: textColour,
+                fontSize: 28,
+                width: 240,
+                height: 60
+              }}>
+              Private Match
+            </button>
+          </div>
+          <br style={{clear: 'both'}} />
+        </div>
       </div>
     );
   }
